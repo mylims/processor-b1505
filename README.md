24a674651ea02697c2cae368bcacd1c9f17cbb3c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-10-04 10:42:39 +0200

    refactor: move from Adonis to raw CLI (#1)
    * chore: apply style
    
    * feat: data process flow
    
    * wip
    
    * refactor: move from Adonis

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # processor-b1505
-Processor for B1505 instrument result
+
+[![build status][ci-image]][ci-url]
+
+Processor for B1505 instrument.
+
+## Usage
+
+First time please be sure to have everything properly configured:
+
+```bash
+cp .env.example .env
+npm install
+npm run tsc
+```
+
+After is only needed to optionally specify the verbosity or interval in seconds for running again the processor (if not specified, it will run once):
+
+```bash
+node ./lib/index.js --verbose --interval 60
+```
+
+## License
+
+[MIT](./LICENSE)
+
+[ci-image]: https://github.com/mylims/processor-b1505/workflows/Node.js%20CI/badge.svg?branch=main
+[ci-url]: https://github.com/mylims/processor-b1505/actions?query=workflow%3A%22Node.js+CI%22
---
+++ src/types.ts
@@ -0,0 +1,54 @@
+// Event related types
+export enum EventStatus {
+  PENDING = 'pending',
+  PROCESSING = 'processing',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+export enum EventDataType {
+  FILE = 'file',
+}
+
+export interface EventProcessor {
+  processorId: string;
+  history: EventHistory[];
+}
+
+export interface EventHistory {
+  processId: string;
+  status: EventStatus;
+  date: Date;
+  message?: string;
+}
+
+export interface EventData {
+  type: EventDataType;
+  fileId: string;
+}
+
+export interface Event {
+  _id: string;
+  topic: string;
+  data: EventData;
+  createdAt: Date;
+  processors: EventProcessor[];
+}
+
+// Processor related types
+export interface ProcessorParams {
+  interval?: number;
+  verbose: boolean;
+}
+
+export type ProcessorType = (
+  /**
+   * Raw file content
+   */
+  content: string,
+  filename: string,
+) => Array<{
+  file?: string;
+  derived?: Record<string, string>;
+  sampleId: string;
+  filename: string;
+}>;
