069eb20a3427c1c062e32c53f680ac72ef174d3c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-10-21 16:22:04 +0200

    feat: process transfer files

---
+++ src/processTransfer.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+
+import { processorCli } from '@mylims/base-processor';
+import type { ProcessorType } from '@mylims/base-processor/lib/types';
+import { fromTransfer, toJcamp } from 'iv-spectrum';
+
+const processB1505: ProcessorType = (content, filename, username) => {
+  const value = content.toString();
+  const analyses = fromTransfer(value);
+  const fileName = path.basename(filename);
+  const sampleCode = [/_#(?<code>)__/.exec(filename)?.groups?.code ?? fileName];
+  return analyses.map((analysis) => {
+    const file = toJcamp(analysis);
+    const { meta = {} } = analysis.getXYSpectrum() ?? {};
+    const { thresholdVoltage, subthresholdSlope } = meta;
+    return {
+      file,
+      sampleCode,
+      filename: `${fileName}.jdx`,
+      username: username ?? 'transfer_test',
+      derived:
+        thresholdVoltage && subthresholdSlope
+          ? { thresholdVoltage, subthresholdSlope }
+          : undefined,
+    };
+  });
+};
+
+processorCli(processB1505);
