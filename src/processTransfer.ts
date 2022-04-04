9c20c16784474e92534a1fb191b71a0908efad11
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-04-04 14:08:44 +0200

    wip

--- src/processTransfer.ts
+++ src/processTransfer.ts
@@ -4,13 +4,12 @@ import { processorCli } from '@mylims/base-processor';
 import type { ProcessorType } from '@mylims/base-processor/lib/types';
 import { fromTransfer, toJcamp } from 'iv-spectrum';
 
-const processB1505: ProcessorType = (content, filename, username) => {
+const processTransfer: ProcessorType = (content, filename, username) => {
   const value = content.toString();
   const analyses = fromTransfer(value);
   const fileName = path.basename(filename);
-  const sampleCode = [
-    /_#(?<code>.+?)__/.exec(filename)?.groups?.code ?? fileName,
-  ];
+  const title = /_#(?<code>.+?)__/.exec(filename)?.groups?.code ?? fileName;
+  const sampleCode = ['#0000', 'A4'];
   return analyses.map((analysis) => {
     const file = toJcamp(analysis);
     const { meta = {} } = analysis.getXYSpectrum() ?? {};
@@ -18,6 +17,7 @@ const processB1505: ProcessorType = (content, filename, username) => {
     return {
       file,
       sampleCode,
+      title,
       filename: `${fileName}.jdx`,
       username: username ?? 'transfer_test',
       derived:
@@ -28,4 +28,4 @@ const processB1505: ProcessorType = (content, filename, username) => {
   });
 };
 
-processorCli(processB1505);
+processorCli(processTransfer);
